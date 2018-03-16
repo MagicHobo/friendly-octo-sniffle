d5d300d45d7e944234a37edf7964c0646ae1f7bb
MagicHobo <37377839+MagicHobo@users.noreply.github.com>
2018-03-15 22:11:05 -0500

    Initial committ.

---
+++ index.js
@@ -0,0 +1,31 @@
+/*var numbers = [1, 2, "balls", 4];
+for (var i = 0; i < numbers.length; i++){
+    var value = numbers[i];
+    console.log("number: " + value);
+}
+
+
+numbers.forEach((numb) => {
+    console.log(numb);
+});
+
+*/
+
+var people = [
+	{ name: "Dan", age: 31 },
+	{ name: "Erin", age: 30.4 },
+	{ name: "Eleanor", age: .5 }
+];
+
+function log(person){
+    console.log("Hello " + person.name);
+}
+
+people.forEach(log);
+
+//people.forEach((numb, index) => {
+//    console.log(numb.name + index);
+//});
+
+var potato = document.getElementById("message");
+potato.innerHTML = "tomato";
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var app = express();
+app.get("/", (request, response)=>{
+    response.sendFile(__dirname + "./index.html");
+});
+app.listen(process.env.PORT || 3000);

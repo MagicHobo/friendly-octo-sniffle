631a684d38150779b4a1b7a6991135ca9b067a91
MagicHobo <37377839+MagicHobo@users.noreply.github.com>
2018-03-16 13:44:06 -0500

    moved some things here and there

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+app.use(express.static(__dirname + "/src"));
+/*
 app.get("/", (request, response)=>{
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/src/index.html");
 });
+*/
 app.listen(process.env.PORT || 3000);

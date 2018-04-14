ad082042e5684c5a060899ef34c35a70a887eb32
MagicHobo <duchateau.t@gmail.com>
2018-04-14 13:17:51 -0500

    remove merge lines

--- server.js
+++ server.js
@@ -7,8 +7,4 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/src/index.html");
 });
 
-<<<<<<< HEAD
 app.listen(process.env.PORT || 6000);
-=======
-app.listen(process.env.PORT || 7000);
->>>>>>> 6893688c16ae5e98c559e202a02a178f37ce4b0c
